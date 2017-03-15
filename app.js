edec3de7bb8b63464b8202834e81f014eb9d0b2c
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-03-15 21:01:28 +0000

    cross and nought player dependent

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ $( function() {
   //   $(this).toggleClass("cross");
   // } );
 
-  var player = 2;
+  var player = 1;
 
   $('td').click( function() {
     if (player === 1) {
       $(this).toggleClass("cross");
+      player = 2;
     } else {
       $(this).toggleClass("circle");
+      player = 1;
     }
   });
 } );
