0571d8f79ecb96b07df871640c93183ef1a806e6
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-03-16 13:31:31 +0000

    find by id to check whats in box

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 $( function() {
-  // $('td').click( function() {
-  //   $(this).toggleClass("cross");
-  // } );
 
   var player = 1;
 
   $('td').click( function() {
+    td = $(this);
+
     if (player === 1) {
-      $(this).toggleClass("cross");
+      $(this).attr('id', 'cross');
       player = 2;
+      isWinner();
     } else {
-      $(this).toggleClass("circle");
+      $(this).attr('id', 'circle');
       player = 1;
+      isWinner();
     }
   });
-} );
+
+  function isWinner() {
+    if ((td[0]).id == 'cross') {
+      console.log('cross');
+} else {
+  console.log('circle');
+}
+  }
+});
+
+
+// write logic for winning
+//
+// if table1 == toggleclass("cross") && table2 == toggleclass("cross") etc
+//
+// write reset button that .removeClass them all
